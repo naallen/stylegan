f0ec3f57e533808560152175c843d58063601ccb
Nicholas Allen <contact@nicholasallen.me>
2020-07-08 18:56:42 +0200

    synd

--- train.py
+++ train.py
@@ -18,23 +18,40 @@ from metrics import metric_base
 # Official training configs for StyleGAN, targeted mainly for FFHQ.
 
 if 1:
-    desc          = 'sgan'                                                                 # Description string included in result subdir name.
-    train         = EasyDict(run_func_name='training.training_loop.training_loop')         # Options for training loop.
-    G             = EasyDict(func_name='training.networks_stylegan.G_style')               # Options for generator network.
-    D             = EasyDict(func_name='training.networks_stylegan.D_basic')               # Options for discriminator network.
-    G_opt         = EasyDict(beta1=0.0, beta2=0.99, epsilon=1e-8)                          # Options for generator optimizer.
-    D_opt         = EasyDict(beta1=0.0, beta2=0.99, epsilon=1e-8)                          # Options for discriminator optimizer.
-    G_loss        = EasyDict(func_name='training.loss.G_logistic_nonsaturating')           # Options for generator loss.
-    D_loss        = EasyDict(func_name='training.loss.D_logistic_simplegp', r1_gamma=10.0) # Options for discriminator loss.
-    dataset       = EasyDict()                                                             # Options for load_dataset().
-    sched         = EasyDict()                                                             # Options for TrainingSchedule.
-    grid          = EasyDict(size='4k', layout='random')                                   # Options for setup_snapshot_image_grid().
-    metrics       = [metric_base.fid50k]                                                   # Options for MetricGroup.
-    submit_config = dnnlib.SubmitConfig()                                                  # Options for dnnlib.submit_run().
-    tf_config     = {'rnd.np_random_seed': 1000}                                           # Options for tflib.init_tf().
+    # Description string included in result subdir name.
+    desc = 'sgan'
+    # Options for training loop.
+    train = EasyDict(run_func_name='training.training_loop.training_loop')
+    # Options for generator network.
+    G = EasyDict(func_name='training.networks_stylegan.G_style')
+    # Options for discriminator network.
+    D = EasyDict(func_name='training.networks_stylegan.D_basic')
+    # Options for generator optimizer.
+    G_opt = EasyDict(beta1=0.0, beta2=0.99, epsilon=1e-8)
+    # Options for discriminator optimizer.
+    D_opt = EasyDict(beta1=0.0, beta2=0.99, epsilon=1e-8)
+    # Options for generator loss.
+    G_loss = EasyDict(func_name='training.loss.G_logistic_nonsaturating')
+    # Options for discriminator loss.
+    D_loss = EasyDict(
+        func_name='training.loss.D_logistic_simplegp', r1_gamma=10.0)
+    # Options for load_dataset().
+    dataset = EasyDict()
+    # Options for TrainingSchedule.
+    sched = EasyDict()
+    # Options for setup_snapshot_image_grid().
+    grid = EasyDict(size='4k', layout='random')
+    # Options for MetricGroup.
+    metrics = [metric_base.fid50k]
+    # Options for dnnlib.submit_run().
+    submit_config = dnnlib.SubmitConfig()
+    # Options for tflib.init_tf().
+    tf_config = {'rnd.np_random_seed': 1000}
 
     # Dataset.
-    desc += '-ffhq';     dataset = EasyDict(tfrecord_dir='ffhq');                 train.mirror_augment = True
+    desc += '-osrsicons'
+    dataset = EasyDict(tfrecord_dir='osrsicons', resolution=64)
+    #train.mirror_augment = True
     #desc += '-ffhq512';  dataset = EasyDict(tfrecord_dir='ffhq', resolution=512); train.mirror_augment = True
     #desc += '-ffhq256';  dataset = EasyDict(tfrecord_dir='ffhq', resolution=256); train.mirror_augment = True
     #desc += '-celebahq'; dataset = EasyDict(tfrecord_dir='celebahq');             train.mirror_augment = True
@@ -43,10 +60,11 @@ if 1:
     #desc += '-cat';      dataset = EasyDict(tfrecord_dir='lsun-cat-full');        train.mirror_augment = False
 
     # Number of GPUs.
-    #desc += '-1gpu'; submit_config.num_gpus = 1; sched.minibatch_base = 4; sched.minibatch_dict = {4: 128, 8: 128, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8, 512: 4}
+    desc += '-1gpu'; submit_config.num_gpus = 1; sched.minibatch_base = 4; sched.minibatch_dict = {
+        4: 128, 8: 128, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8, 512: 4}
     #desc += '-2gpu'; submit_config.num_gpus = 2; sched.minibatch_base = 8; sched.minibatch_dict = {4: 256, 8: 256, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8}
     #desc += '-4gpu'; submit_config.num_gpus = 4; sched.minibatch_base = 16; sched.minibatch_dict = {4: 512, 8: 256, 16: 128, 32: 64, 64: 32, 128: 16}
-    desc += '-8gpu'; submit_config.num_gpus = 8; sched.minibatch_base = 32; sched.minibatch_dict = {4: 512, 8: 256, 16: 128, 32: 64, 64: 32}
+    #desc += '-8gpu'; submit_config.num_gpus = 8; sched.minibatch_base = 32; sched.minibatch_dict = {4: 512, 8: 256, 16: 128, 32: 64, 64: 32}
 
     # Default options.
     train.total_kimg = 25000
@@ -62,12 +80,12 @@ if 1:
     #desc += '-add-mapping-and-styles'; G.const_input_layer = False; G.style_mixing_prob = 0.0; G.use_noise = False
     #desc += '-remove-traditional-input'; G.style_mixing_prob = 0.0; G.use_noise = False
     #desc += '-add-noise-inputs'; G.style_mixing_prob = 0.0
-    #desc += '-mixing-regularization' # default
+    # desc += '-mixing-regularization' # default
 
     # Table 2.
     #desc += '-mix0'; G.style_mixing_prob = 0.0
     #desc += '-mix50'; G.style_mixing_prob = 0.5
-    #desc += '-mix90'; G.style_mixing_prob = 0.9 # default
+    # desc += '-mix90'; G.style_mixing_prob = 0.9 # default
     #desc += '-mix100'; G.style_mixing_prob = 1.0
 
     # Table 4.
@@ -76,29 +94,45 @@ if 1:
     #desc += '-stylebased-0'; G.mapping_layers = 0
     #desc += '-stylebased-1'; G.mapping_layers = 1
     #desc += '-stylebased-2'; G.mapping_layers = 2
-    #desc += '-stylebased-8'; G.mapping_layers = 8 # default
+    # desc += '-stylebased-8'; G.mapping_layers = 8 # default
 
 #----------------------------------------------------------------------------
 # Official training configs for Progressive GAN, targeted mainly for CelebA-HQ.
 
 if 0:
-    desc          = 'pgan'                                                         # Description string included in result subdir name.
-    train         = EasyDict(run_func_name='training.training_loop.training_loop') # Options for training loop.
-    G             = EasyDict(func_name='training.networks_progan.G_paper')         # Options for generator network.
-    D             = EasyDict(func_name='training.networks_progan.D_paper')         # Options for discriminator network.
-    G_opt         = EasyDict(beta1=0.0, beta2=0.99, epsilon=1e-8)                  # Options for generator optimizer.
-    D_opt         = EasyDict(beta1=0.0, beta2=0.99, epsilon=1e-8)                  # Options for discriminator optimizer.
-    G_loss        = EasyDict(func_name='training.loss.G_wgan')                     # Options for generator loss.
-    D_loss        = EasyDict(func_name='training.loss.D_wgan_gp')                  # Options for discriminator loss.
-    dataset       = EasyDict()                                                     # Options for load_dataset().
-    sched         = EasyDict()                                                     # Options for TrainingSchedule.
-    grid          = EasyDict(size='1080p', layout='random')                        # Options for setup_snapshot_image_grid().
-    metrics       = [metric_base.fid50k]                                           # Options for MetricGroup.
-    submit_config = dnnlib.SubmitConfig()                                          # Options for dnnlib.submit_run().
-    tf_config     = {'rnd.np_random_seed': 1000}                                   # Options for tflib.init_tf().
+    # Description string included in result subdir name.
+    desc = 'pgan'
+    # Options for training loop.
+    train = EasyDict(run_func_name='training.training_loop.training_loop')
+    # Options for generator network.
+    G = EasyDict(func_name='training.networks_progan.G_paper')
+    # Options for discriminator network.
+    D = EasyDict(func_name='training.networks_progan.D_paper')
+    # Options for generator optimizer.
+    G_opt = EasyDict(beta1=0.0, beta2=0.99, epsilon=1e-8)
+    # Options for discriminator optimizer.
+    D_opt = EasyDict(beta1=0.0, beta2=0.99, epsilon=1e-8)
+    # Options for generator loss.
+    G_loss = EasyDict(func_name='training.loss.G_wgan')
+    # Options for discriminator loss.
+    D_loss = EasyDict(func_name='training.loss.D_wgan_gp')
+    # Options for load_dataset().
+    dataset = EasyDict()
+    # Options for TrainingSchedule.
+    sched = EasyDict()
+    # Options for setup_snapshot_image_grid().
+    grid = EasyDict(size='1080p', layout='random')
+    # Options for MetricGroup.
+    metrics = [metric_base.fid50k]
+    # Options for dnnlib.submit_run().
+    submit_config = dnnlib.SubmitConfig()
+    # Options for tflib.init_tf().
+    tf_config = {'rnd.np_random_seed': 1000}
 
     # Dataset (choose one).
-    desc += '-celebahq';            dataset = EasyDict(tfrecord_dir='celebahq'); train.mirror_augment = True
+    desc += '-celebahq'
+    dataset = EasyDict(tfrecord_dir='celebahq')
+    train.mirror_augment = True
     #desc += '-celeba';              dataset = EasyDict(tfrecord_dir='celeba'); train.mirror_augment = True
     #desc += '-cifar10';             dataset = EasyDict(tfrecord_dir='cifar10')
     #desc += '-cifar100';            dataset = EasyDict(tfrecord_dir='cifar100')
@@ -138,14 +172,15 @@ if 0:
     #desc += '-lsun-tvmonitor';      dataset = EasyDict(tfrecord_dir='lsun-tvmonitor-100k');      train.mirror_augment = True
 
     # Conditioning & snapshot options.
-    #desc += '-cond'; dataset.max_label_size = 'full' # conditioned on full label
-    #desc += '-cond1'; dataset.max_label_size = 1 # conditioned on first component of the label
+    # desc += '-cond'; dataset.max_label_size = 'full' # conditioned on full label
+    # desc += '-cond1'; dataset.max_label_size = 1 # conditioned on first component of the label
     #desc += '-g4k'; grid.size = '4k'
     #desc += '-grpc'; grid.layout = 'row_per_class'
 
     # Config presets (choose one).
     #desc += '-preset-v1-1gpu'; submit_config.num_gpus = 1; D.mbstd_group_size = 16; sched.minibatch_base = 16; sched.minibatch_dict = {256: 14, 512: 6, 1024: 3}; sched.lod_training_kimg = 800; sched.lod_transition_kimg = 800; train.total_kimg = 19000
-    desc += '-preset-v2-1gpu'; submit_config.num_gpus = 1; sched.minibatch_base = 4; sched.minibatch_dict = {4: 128, 8: 128, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8, 512: 4}; sched.G_lrate_dict = {1024: 0.0015}; sched.D_lrate_dict = EasyDict(sched.G_lrate_dict); train.total_kimg = 12000
+    desc += '-preset-v2-1gpu'; submit_config.num_gpus = 1; sched.minibatch_base = 4; sched.minibatch_dict = {
+        4: 128, 8: 128, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8, 512: 4}; sched.G_lrate_dict = {1024: 0.0015}; sched.D_lrate_dict = EasyDict(sched.G_lrate_dict); train.total_kimg = 12000
     #desc += '-preset-v2-2gpus'; submit_config.num_gpus = 2; sched.minibatch_base = 8; sched.minibatch_dict = {4: 256, 8: 256, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8}; sched.G_lrate_dict = {512: 0.0015, 1024: 0.002}; sched.D_lrate_dict = EasyDict(sched.G_lrate_dict); train.total_kimg = 12000
     #desc += '-preset-v2-4gpus'; submit_config.num_gpus = 4; sched.minibatch_base = 16; sched.minibatch_dict = {4: 512, 8: 256, 16: 128, 32: 64, 64: 32, 128: 16}; sched.G_lrate_dict = {256: 0.0015, 512: 0.002, 1024: 0.003}; sched.D_lrate_dict = EasyDict(sched.G_lrate_dict); train.total_kimg = 12000
     #desc += '-preset-v2-8gpus'; submit_config.num_gpus = 8; sched.minibatch_base = 32; sched.minibatch_dict = {4: 512, 8: 256, 16: 128, 32: 64, 64: 32}; sched.G_lrate_dict = {128: 0.0015, 256: 0.002, 512: 0.003, 1024: 0.003}; sched.D_lrate_dict = EasyDict(sched.G_lrate_dict); train.total_kimg = 12000
@@ -174,17 +209,22 @@ if 0:
 # Main entry point for training.
 # Calls the function indicated by 'train' using the selected options.
 
+
 def main():
     kwargs = EasyDict(train)
-    kwargs.update(G_args=G, D_args=D, G_opt_args=G_opt, D_opt_args=D_opt, G_loss_args=G_loss, D_loss_args=D_loss)
-    kwargs.update(dataset_args=dataset, sched_args=sched, grid_args=grid, metric_arg_list=metrics, tf_config=tf_config)
+    kwargs.update(G_args=G, D_args=D, G_opt_args=G_opt,
+                  D_opt_args=D_opt, G_loss_args=G_loss, D_loss_args=D_loss)
+    kwargs.update(dataset_args=dataset, sched_args=sched,
+                  grid_args=grid, metric_arg_list=metrics, tf_config=tf_config)
     kwargs.submit_config = copy.deepcopy(submit_config)
-    kwargs.submit_config.run_dir_root = dnnlib.submission.submit.get_template_from_path(config.result_dir)
+    kwargs.submit_config.run_dir_root = dnnlib.submission.submit.get_template_from_path(
+        config.result_dir)
     kwargs.submit_config.run_dir_ignore += config.run_dir_ignore
     kwargs.submit_config.run_desc = desc
     dnnlib.submit_run(**kwargs)
 
 #----------------------------------------------------------------------------
+
 
 if __name__ == "__main__":
     main()
